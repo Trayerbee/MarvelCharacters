692fb18b3ccb092ca678d7772e24fb336b06c0b7
ilyas-accenture <ilyas.karshigabekov@accenture.com>
2018-10-10 15:09:51 +0300

    Added all necessary modules. Added necessary entities. Added first user interface files.

---
+++ App/CharactersListPresenter.swift
@@ -0,0 +1,17 @@
+//
+//  CharactersListPresenter.swift
+//  Interface
+//
+//  Created by Karshigabekov, Ilyas on 10/10/2018.
+//  Copyright © 2018 Karshigabekov, Ilyas. All rights reserved.
+//
+
+import Foundation
+protocol CharacterListPresenterActions {
+
+}
+
+
+class CharactersListPresenter: CharacterListPresenterActions {
+
+}
---
+++ Transport/Supporting/Transport.h
@@ -0,0 +1,19 @@
+//
+//  Transport.h
+//  Transport
+//
+//  Created by Karshigabekov, Ilyas on 10/10/2018.
+//  Copyright © 2018 Karshigabekov, Ilyas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Transport.
+FOUNDATION_EXPORT double TransportVersionNumber;
+
+//! Project version string for Transport.
+FOUNDATION_EXPORT const unsigned char TransportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Transport/PublicHeader.h>
+
+
